50b20c5c554b332fa0b4ea46233794036adaed00
shlomi noyshtat <s0583244814@gmail.com>
2025-09-16 12:48:46 +0300

    Client connection to server completed.

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,16 +1,38 @@
 import { useNavigate } from "react-router";
+import React from "react";
 import '../style/login.css';
 
 export default function Login() {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState(""); 
   let navigate = useNavigate();
+
+    const onsubmit = async () => {
+        console.log(username, password  );
+        const data = await fetch("http://localhost:6578/users/checkUser",
+            {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username, password}),
+            }); 
+        const result = await data.json();
+        console.log(result.status);
+        if (result.status) {
+            navigate("/homepage");
+        } else {
+            alert("Invalid credentials, please try again.");
+        }
+    }   
+
+
   return (
     <>
       <main>
-        <img src="אמן.png" alt="" id="haman" />
+        <img src="Aman.png" alt="" id="haman" />
         <section id="sectionInput">
-          <input type="text" placeholder="enter your name" />
-          <input type="text" placeholder="enter password" />
-          <button onClick={() => navigate("/homePage")}>Log in</button>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="enter your name" />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="enter password" />
+          <button onClick={onsubmit}>Log in</button>
         </section>
         <p>!סודי ביותר</p>
       </main>
