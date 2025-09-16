a9e001e00c4c85941bafc9ad4105f88bf33b91eb
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-16 15:26:10 +0300

    save role

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,37 +1,48 @@
 import { useNavigate } from "react-router";
-import React from "react";
-import '../style/login.css';
+import React, { useContext } from "react";
+import "../style/login.css";
+import { RoleContext } from "../contexts/role.context";
 
+const role = useContext(RoleContext);
 export default function Login() {
-    const [username, setUsername] = React.useState("");
-    const [password, setPassword] = React.useState(""); 
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
   let navigate = useNavigate();
 
-    const onsubmit = async () => {
-        console.log(username, password  );
-        const data = await fetch("http://localhost:6578/users/checkUser",
-            {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({username, password}),
-            }); 
-        const result = await data.json();
-        console.log(result.status);
-        if (result.status) {
-            navigate("/homepage");
-        } else {
-            alert("Invalid credentials, please try again.");
-        }
-    }   
-
+  const onsubmit = async () => {
+    console.log(username, password);
+    const data = await fetch("http://localhost:6578/users/checkUser", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    const result = await data.json();
+    role?.setRole(result.role);
+    console.log(result.status);
+    if (result.status) {
+      navigate("/homepage");
+    } else {
+      alert("Invalid credentials, please try again.");
+    }
+  };
 
   return (
     <>
       <main>
         <img src="Aman.png" alt="" id="haman" />
         <section id="sectionInput">
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="enter your name" />
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="enter password" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="enter your name"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="enter password"
+          />
           <button onClick={onsubmit}>Log in</button>
         </section>
         <p>!סודי ביותר</p>
---
+++ src/contexts/role.context.tsx
@@ -0,0 +1,25 @@
+import {
+  createContext,
+  useState,
+  type Dispatch,
+  type PropsWithChildren,
+  type SetStateAction,
+} from "react";
+
+export const RoleContext = createContext<RoleContextType | null>(null);
+
+type RoleContextType = {
+  role: string;
+  setRole: Dispatch<SetStateAction<string>>;
+};
+
+function RoleProvider({ children }: PropsWithChildren) {
+  const [role, setRole] = useState<string>("chaim");
+  return (
+    <RoleContext.Provider value={{ role, setRole }}>
+      {children}
+    </RoleContext.Provider>
+  );
+}
+
+export default RoleProvider;
