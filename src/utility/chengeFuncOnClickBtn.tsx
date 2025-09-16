8237e796ce2007eb51b4f8c391af8aa7025650fe
Moti7950 <shalom7950@gmail.com>
2025-09-16 16:07:58 +0300

    Fix button

---
+++ src/utility/chengeFuncOnClickBtn.tsx
@@ -0,0 +1,19 @@
+export  function ZeekLive(){
+    console.log("from ZeekLive function"); 
+}
+
+export  function SoldierLocations(){
+    console.log("from soldier locations function"); 
+}
+
+export  function TargetOfficer(){
+    console.log("from TargetOfficer function"); 
+}
+
+export  function MarkSuspicious(){
+    console.log("from MarkSuspicious function"); 
+}
+
+export  function ComparingWithAI(){
+    console.log("from ComparingWithAI function"); 
+}
