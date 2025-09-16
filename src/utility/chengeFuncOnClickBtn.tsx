61e767b9bbc474d75ba5138cebbfa02c22d121a7
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-16 18:08:56 +0300

    get soldiers

--- src/utility/chengeFuncOnClickBtn.tsx
+++ src/utility/chengeFuncOnClickBtn.tsx
@@ -1,19 +1,28 @@
-export  function ZeekLive(){
-    console.log("from ZeekLive function"); 
+import type { location } from "../types/location";
+
+export function ZeekLive() {
+  console.log("from ZeekLive function");
 }
 
-export  function SoldierLocations(){
-    console.log("from soldier locations function"); 
+export async function SoldierLocations(setLocations: (locs: location[]) => void) {
+  try {
+    const res = await fetch("http://localhost:6578/locations");
+    const data: location[] = await res.json();
+    const soldierLocations = data.filter((loc) => loc.type === "soldier");
+    setLocations(soldierLocations);
+  } catch (err) {
+    console.error("שגיאה בטעינת נתונים:", err);
+  }
 }
 
-export  function TargetOfficer(){
-    console.log("from TargetOfficer function"); 
+export function TargetOfficer() {
+  console.log("from TargetOfficer function");
 }
 
-export  function MarkSuspicious(){
-    console.log("from MarkSuspicious function"); 
+export function MarkSuspicious() {
+  console.log("from MarkSuspicious function");
 }
 
-export  function ComparingWithAI(){
-    console.log("from ComparingWithAI function"); 
+export function ComparingWithAI() {
+  console.log("from ComparingWithAI function");
 }
