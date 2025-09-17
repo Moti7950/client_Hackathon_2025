7bb1e0056c4918585a40dd82dcd501d144577d87
Yosef Steinberg <steinbergyosef@gmail.com>
2025-09-17 12:50:59 +0300

    Fix component dron

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Login from "./components/login";
-import ZeekLive from "./components/zeekLive";
+import DroneMap from "./components/zeekLive";
 import HomePage from "./components/home-page";
 import MapView from "./components/MapView";
 import ProtectedRoute from "./contexts/ProtectedRoute";
+
 
 export default function App() {
   return (
@@ -15,7 +16,7 @@ export default function App() {
       <Route element={<ProtectedRoute />}>
         <Route path="/homePage" element={<HomePage />}>
           <Route index element={<MapView />} />     {/* /homePage */}
-          <Route path="zeek" element={<ZeekLive />} /> {/* /homePage/zeek */}
+          <Route path="zeek" element={<DroneMap />} /> {/* /homePage/zeek */}
         </Route>
       </Route>
 
