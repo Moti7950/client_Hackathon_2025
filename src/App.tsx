b1380826eb84c605a6d3b675cb4802a8267fd6f7
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-17 10:50:38 +0300

    locations in zeek

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Login from './components/login'
-import GazaZoomMap from './components/gazaStrip';
+import RoleProvider from './contexts/role.context';
 function App() {
   return (
       <RoleProvider>
--- src/components/gazaStrip.tsx
+++ src/components/gazaStrip.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import { MapContainer, TileLayer, Polygon } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from "react";
+import { MapContainer, TileLayer, Polygon, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useLocations } from "../contexts/locations.context";
+import * as turf from "@turf/turf";
 
 const smallGazaPolygon: [number, number][] = [
-  [31.5105, 34.4730],
+  [31.5105, 34.473],
   [31.5105, 34.4735],
-  [31.5100, 34.4735],
-  [31.5100, 34.4730],
-  [31.5105, 34.4730] // Closing the loop
+  [31.51, 34.4735],
+  [31.51, 34.473],
+  [31.5105, 34.473], // Closing the loop
 ];
 
 const GazaZoomMap: React.FC = () => {
+  const { locations } = useLocations();
+  const polygon = turf.polygon([
+    [...smallGazaPolygon.map(([lat, lng]) => [lng, lat])],
+  ]);
+  const filteredLocations = locations.filter((loc) => {
+    const point = turf.point([loc.len, loc.lat]);
+    return turf.booleanPointInPolygon(point, polygon);
+  });
   return (
     <MapContainer
-      center={[31.51025, 34.47325]} 
-      zoom={20} 
-      style={{ height: '500px', width: '100%' }}
+      center={[31.51025, 34.47325]}
+      zoom={20}
+      style={{ height: "500px", width: "100%" }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -23,8 +33,14 @@ const GazaZoomMap: React.FC = () => {
       />
       <Polygon
         positions={smallGazaPolygon}
-        pathOptions={{ color: 'red', fillOpacity: 0.4 }}
+        pathOptions={{ color: "red", fillOpacity: 0.4 }}
       />
+
+      {filteredLocations.map((loc) => (
+        <Marker key={loc.id} position={[loc.lat, loc.len]}>
+          <Popup>{loc.description}</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
